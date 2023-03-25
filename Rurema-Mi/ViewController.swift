17e8c7522aad10a8de717a44c123cb26c050596c
motoakira <motoakira@mac.com>
2023-03-25 16:29:08 +0900

    Initial Commit

---
+++ Rurema-Mi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rurema-Mi
+//
+//  Created by 進藤 元明 on 2023/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
