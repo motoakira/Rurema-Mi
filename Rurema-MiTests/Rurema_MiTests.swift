a8d08d4110f2c740a1710bbb75f4c8f891c29fc5
motoakira <motoakira@mac.com>
2024-03-29 16:20:34 +0900

    Cancel Contsraints. Waiting viewController.webView in Rurema_MiTests.swift

--- Rurema-MiTests/Rurema_MiTests.swift
+++ Rurema-MiTests/Rurema_MiTests.swift
@@ -16,6 +16,7 @@ final class Rurema_MiTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let viewController = ViewController()
+        while ( viewController.webView == nil ) {}
         viewController.viewDidLoad()
     }
 
